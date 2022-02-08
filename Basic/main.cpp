582a79c5bb02e7ad133c54c4bbf261e7f4b7ac4a
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-02-08 18:48:42 +0530

    Add files via upload

---
+++ Basic/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout<<"Enter any string:";
+   // cin>>str;
+   getline(cin, str);
+
+    cout<<"You entered:"<<str<<endl;
+
+//    int a = 10;
+
+//    cout<<sizeof(int)<<endl;
+
+    return 0;
+}
