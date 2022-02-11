d8c6dddc935722c8b30ca5e5b226df322d80d44f
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-02-11 18:58:14 +0530

    Add files via upload

---
+++ DecisionMaking-1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+//Decision Making or Conditional Statements
+//
+//1. If-else
+//2. If-else if -else
+//3. Switch
+//4. Ternary Operator shorthand of if-else
+
+int main()
+{
+    int n;
+
+    cout<<"Enter any number:";
+    cin>>n;
+
+    if(n % 2 == 0)
+    {
+        cout<<"Number is Even";
+    }
+//    else {
+//        cout<<"Number is Odd";
+//    }
+
+    return 0;
+}
